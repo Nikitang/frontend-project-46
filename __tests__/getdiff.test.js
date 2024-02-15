bebcc6e74507e7ada24d630bb46f212f35a9ed1a
Nikita <gerasimenya.05@mail.ru>
2024-02-15 13:55:04 +0300

    codeClimate

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -1,17 +1,27 @@
-import {describe, test, expect} from '@jest/globals';
+
 import parseData from '../dataParse.js';
+import getDiff from '../src/getDiff.js';
 
 describe('my beverage', () => {
+    test('parseData', () => {
+        expect(parseData("file1.json")).toEqual({
+            "host": "hexlet.io",
+            "timeout": 50,
+            "proxy": "123.234.53.22",
+            "follow": false
+          })
+    })
 
     test('gendiff', () => {
-        const res = parseData(file1);
-        const res2 = parseData(file2);
+        const res = parseData("file1.json");
+        const res2 = parseData("file2.json");
         expect(getDiff(res, res2)).toEqual(`{
-            - follow: false
-            host: hexlet.io
-            - proxy: 123.234.53.22
-            - timeout: 50
-            + timeout: 20
-            + verbose: true}`)
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`)
     });
 });
